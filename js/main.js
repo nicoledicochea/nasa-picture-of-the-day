57a35014c08e054448e099396243b7c9be560a55
Nicole Dicochea <dicocheanicole@gmail.com>
2022-10-17 20:38:56 -0700

    added search by date, changed page title, added club name, fixed embedded videos

--- js/main.js
+++ js/main.js
@@ -2,17 +2,35 @@
 
 document.querySelector('button').addEventListener('click', getFetch)
 
+document.querySelector('iframe').style.display = 'none'
+document.querySelector('div').style.display = 'none'
+document.querySelector('img').style.display = 'none'
+
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://api.nasa.gov/planetary/apod?api_key=hd9r2asEQdy1oQRXRnRfahXblLW7cAfvIvDgZwod'
+  console.log(choice)
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=hd9r2asEQdy1oQRXRnRfahXblLW7cAfvIvDgZwod&date=${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('h2').innerText = data.title
-        document.querySelector('img').src = data.url
+
+        if(data.media_type === 'image') {
+          document.querySelector('img').style.display = 'block'
+          document.querySelector('img').src = data.url
+        } else if(data.media_type === 'video') {
+          document.querySelector('img').style.display = 'none'
+          document.querySelector('div').style.display = 'block'
+          document.querySelector('iframe').style.display = 'block'
+          document.querySelector('iframe').src = data.url
+        }
+
+        document.querySelector('#name').innerText = data.title
         document.querySelector('p').innerText = data.explanation
+        document.querySelector('h3').innerText = data.date
+        
       })
       .catch(err => {
           console.log(`error ${err}`)
